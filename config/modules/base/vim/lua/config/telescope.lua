ed0ce0174dcedfb50560f01614b503e26b3aebc7
Nathan Azaria <nathanajah@gmail.com>
2025-06-29 10:31:59 -0700

    Add telescope keymap binding

--- config/modules/base/vim/lua/config/telescope.lua
+++ config/modules/base/vim/lua/config/telescope.lua
@@ -20,4 +20,5 @@ require("telescope").setup {
 vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', {})
 vim.api.nvim_set_keymap('n', '<F9>', ':Telescope lsp_dynamic_workspace_symbols<CR>', {})
 vim.cmd('command Rg :Telescope live_grep')
-vim.api.nvim_set_keymap('n', '<Leader>tfh', '<cmd>Telescope find_files follow=true hidden=true<CR>', {})
+vim.api.nvim_set_keymap('n', '<Leader>sfh', '<cmd>Telescope find_files follow=true hidden=true<CR>', {})
+vim.api.nvim_set_keymap('n', '<Leader>sk', '<cmd>Telescope keymaps<CR>', {})
